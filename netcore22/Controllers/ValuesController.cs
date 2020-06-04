52665ced0dc4f9521061acd583bf31b24e491be0
Steven <stevenrodrod@gmail.com>
2020-06-04 16:08:50 -0500

    Actualización con variables de entorno

--- netcore22/Controllers/ValuesController.cs
+++ netcore22/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Configuration;
 
 namespace netcore22.Controllers
 {
@@ -10,11 +12,25 @@ namespace netcore22.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+
+        protected IConfiguration Config;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            this.Config = configuration;
+        }
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", Environment.GetEnvironmentVariable("MESSAGE") };
+            return new string[] {
+                "value1",
+                Environment.GetEnvironmentVariable("MESSAGE"),
+                this.Config.GetSection("Config:value1").Value,
+                this.Config.GetSection("Config:value2").Value,
+                this.Config.GetSection("Config:value3").Value
+            };
         }
 
         // GET api/values/5
